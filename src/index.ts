4023bcd4a13802c257025593b3fb2d4e49ab0dac
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-12-27 15:11:42 +0100

    fix eslint issues and format

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,56 @@
-import { Elysia, t } from "elysia";
-import { swagger } from "@elysiajs/swagger"
-import { nanoid } from "nanoid";
+import { swagger } from '@elysiajs/swagger';
+import { Elysia, t } from 'elysia';
+import { nanoid } from 'nanoid';
 
 // Database mock
-let shortenedUrls = [
+const shortenedUrls = [
   {
     origin: 'github.com/danielroetzer',
     short: 'abcd',
-  }
-]
+  },
+];
 
-const getShortUUID = () => nanoid(12)
+const getShortUUID = () => nanoid(12);
 
 const app = new Elysia()
   .use(swagger())
-  .get("/", () => JSON.stringify(shortenedUrls))
-  .get("/:shortUrl", ({ params, set }) => {
-    const redirectTo = shortenedUrls.find(url => url.short === params.shortUrl)?.origin
+  .get('/', () => JSON.stringify(shortenedUrls))
+  .get(
+    '/:shortUrl',
+    ({ params, set }) => {
+      const redirectTo = shortenedUrls.find(
+        (url) => url.short === params.shortUrl,
+      )?.origin;
 
-    if (!redirectTo) {
-      set.status = 404
+      if (!redirectTo) {
+        set.status = 404;
 
-      return 'URL not found'
-    }
+        return 'URL not found';
+      }
 
-    return Response.redirect(`http://www.${redirectTo}`, 301)
-  }, {
-    params: t.Object({
-      shortUrl: t.String()
-    })
-  })
-  .get("/url/:shortUrl", ({ params }) => shortenedUrls.find(url => url.short === params.shortUrl))
-  .post("/create", ({ body }) => shortenedUrls.push({ origin: body.url, short: getShortUUID()}), {
-    body: t.Object({
-      url: t.String()
-    })
-  })
+      return Response.redirect(`http://www.${redirectTo}`, 301);
+    },
+    {
+      params: t.Object({
+        shortUrl: t.String(),
+      }),
+    },
+  )
+  .get('/url/:shortUrl', ({ params }) =>
+    shortenedUrls.find((url) => url.short === params.shortUrl),
+  )
+  .post(
+    '/create',
+    ({ body }) =>
+      shortenedUrls.push({ origin: body.url, short: getShortUUID() }),
+    {
+      body: t.Object({
+        url: t.String(),
+      }),
+    },
+  )
   .listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
