af6bde7d274dcb6f8930d70c3d7d786a78b566c3
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2023-12-27 12:51:38 +0100

    create basic elysia routes for link shortening

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,40 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
+import { nanoid } from "nanoid";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+// Databas mock
+let shortenedUrls = [
+  {
+    origin: 'github.com/danielroetzer',
+    short: 'abcd',
+  }
+]
+
+const getShortUUID = () => nanoid(12)
+
+const app = new Elysia()
+  .get("/", () => JSON.stringify(shortenedUrls))
+  .get("/:shortUrl", ({ params, set }) => {
+    const redirectTo = shortenedUrls.find(url => url.short === params.shortUrl)?.origin
+
+    if (!redirectTo) {
+      set.status = 404
+
+      return 'URL not found'
+    }
+
+    return Response.redirect(`http://www.${redirectTo}`, 301)
+  }, {
+    params: t.Object({
+      shortUrl: t.String()
+    })
+  })
+  .get("/url/:shortUrl", ({ params }) => shortenedUrls.find(url => url.short === params.shortUrl))
+  .post("/create", ({ body }) => shortenedUrls.push({ origin: body.url, short: getShortUUID()}), {
+    body: t.Object({
+      url: t.String()
+    })
+  })
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
